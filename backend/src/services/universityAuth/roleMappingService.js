2b9636e9acb0e41c160b1a89e1918ff974ba3e37
chamodya-work <186557351+chamodya-work@users.noreply.github.com>
2026-06-22 12:12:58 +0530

    update shs student role mapping --> sh because of api response format

--- backend/src/services/universityAuth/roleMappingService.js
+++ backend/src/services/universityAuth/roleMappingService.js
@@ -21,7 +21,7 @@ export function mapCourseNameFromStudentNumber(studentNumber, apiCourse) {
   const byPrefix = {
     ME: 'MBBS',
     OT: 'OT',
-    SHS: 'SHS',
+    SH: 'SHS',
   };
 
   if (byPrefix[prefix]) return byPrefix[prefix];
